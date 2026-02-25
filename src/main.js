32a1521b90bda30b1717f957649b9cd03d818dc5
teopecci <matteo.pecchi18@gmail.com>
2026-02-25 15:59:01 +0100

    ottimizzazione CDN 1

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from "./App.vue"
 import router from "./router"
 import "./style.css"
 import "leaflet/dist/leaflet.css"
-import '@fortawesome/fontawesome-free/css/all.min.css'
+
 
 createApp(App)
     .use(createPinia())
